1e9ffa32e84258f5c3938bd3b56f388b561c985b
katthartic <39164291+katthartic@users.noreply.github.com>
2020-04-29 18:56:47 -0400

    Katt user & review models (#8)
    * Add id and validation to email
    
    * Add properties
    
    * Add associations for User and Review models
    
    * Add min & max length for description
    
    * Comment out unmade models
    
    * Fix syntax error in rating
    
    * Fix rating type and validation

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,4 +1,7 @@
 const User = require('./user')
+const Review = require('./review')
+const Product = require('./product')
+const Order = require('./order')
 
 /**
  * If we had any associations to make, this would be a great place to put them!
@@ -7,6 +10,11 @@ const User = require('./user')
  *    BlogPost.belongsTo(User)
  */
 
+User.hasMany(Review)
+// User.hasMany(Order)
+Review.belongsTo(User)
+// Review.belongsTo(Product)
+
 /**
  * We'll export all of our models here, so that any time a module needs a model,
  * we can just require it from 'db/models'
@@ -14,5 +22,6 @@ const User = require('./user')
  * instead of: const User = require('../db/models/user')
  */
 module.exports = {
-  User
+  User,
+  Review,
 }
