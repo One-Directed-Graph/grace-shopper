7e93fd9011fe830dbe574a6cf0f65da6c50307b5
katthartic <katt.baum@gmail.com>
2020-04-29 18:42:22 -0400

    Fix rating type and validation

--- server/db/models/review.js
+++ server/db/models/review.js
@@ -17,8 +17,13 @@ const Review = db.define('review', {
     },
   },
   rating: {
-    type: Sequelize.ENUM(1, 2, 3, 4, 5),
+    type: Sequelize.INTEGER,
     allowNull: false,
+    validate: {
+      min: 1,
+      max: 5,
+      notEmpty: true,
+    },
   },
 })
 
