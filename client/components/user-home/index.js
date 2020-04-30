fc718a16c92357a19050bd814d17ac9797d03a33
katthartic <katt.baum@gmail.com>
2020-04-30 19:18:13 -0400

    Make user-home directory

---
+++ client/components/user-home/index.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+import {Link, HashRouter, Route} from 'react-router-dom'
+
+//ASSIGNED TO: Katt
+
+/**
+ * COMPONENT
+ */
+export const UserHome = (props) => {
+  const {email} = props
+  const greetName = email.split('@')
+
+  return (
+    <div id="user-home-container">
+      <h2>My Account</h2>
+      <h3>Welcome back, {greetName}</h3>
+      <hr />
+      <div id="user-home-acct">
+        <nav id="user-home-acct-nav">
+          <Link to="home/orders">Orders</Link>
+          <Link to="home/reviews">Reviews</Link>
+          <Link to="home/addresses">Addresses</Link>
+          <Link to="home/payment-info">Payment Info</Link>
+        </nav>
+        <HashRouter>
+          <Route path="home/orders" />
+          <Route path="home/reviews" />
+        </HashRouter>
+      </div>
+    </div>
+  )
+}
+
+/**
+ * CONTAINER
+ */
+const mapState = (state) => {
+  return {
+    email: state.user.email,
+  }
+}
+
+export default connect(mapState)(UserHome)
+
+/**
+ * PROP TYPES
+ */
+UserHome.propTypes = {
+  email: PropTypes.string,
+}
