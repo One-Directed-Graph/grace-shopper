e77aced2c682ec3e49685c6207f3a2eadf830b42
AleksandrKayner <AleksandrKayner6@gmail.com>
2020-04-30 17:41:18 -0400

    product table populated

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -2,6 +2,7 @@ const User = require('./user')
 const Review = require('./review')
 const Product = require('./product')
 const Order = require('./order')
+const Category = require('./category')
 
 // /**
 //  * If we had any associations to make, this would be a great place to put them!
@@ -9,8 +10,10 @@ const Order = require('./order')
 //  *
 //  *    BlogPost.belongsTo(User)
 //  */
+//Category.belongsTo(Product)
 
-User.hasMany(Review)
+Product.belongsTo(Category)
+//User.hasMany(Review)
 // // User.hasMany(Order)
 // Review.belongsTo(User)
 // // Review.belongsTo(Product)
@@ -27,8 +30,8 @@ User.hasMany(Review)
 // }
 //User to Order relationship
 //Order to Product relationship should be managed in FrontEnd
-User.hasMany(Order)
-Order.belongsTo(User)
+//User.hasMany(Order)
+//Order.belongsTo(User)
 
 /**
  * We'll export all of our models here, so that any time a module needs a model,
@@ -40,5 +43,6 @@ module.exports = {
   User,
   Review,
   Order,
+  Category,
   Product,
 }
