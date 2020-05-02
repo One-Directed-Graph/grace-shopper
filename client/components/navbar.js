66db6ec5b0aee6ab6b312579f4071f827cdd3771
katthartic <katt.baum@gmail.com>
2020-05-01 22:05:56 -0400

    Add load function

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,34 +1,44 @@
-import React from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
-import {logout} from '../store'
+import {logout, getProducts} from '../store'
+// import {render} from 'enzyme' - REMOVE?
 
-const Navbar = ({handleClick, isLoggedIn}) => (
-  <div>
-    <h1>Maskerade</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-          <Link to="/products">Products</Link>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-          <Link to="/products">Products</Link>
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
-)
+class Navbar extends Component {
+  componentDidMount() {
+    this.props.load()
+  }
+
+  render() {
+    const {handleClick, isLoggedIn} = this.props
+    return (
+      <div>
+        <h1>Maskerade</h1>
+        <nav>
+          {isLoggedIn ? (
+            <div>
+              {/* The navbar will show these links after you log in */}
+              <Link to="/home">Home</Link>
+              <a href="#" onClick={handleClick}>
+                Logout
+              </a>
+              <Link to="/products">Products</Link>
+            </div>
+          ) : (
+            <div>
+              {/* The navbar will show these links before you log in */}
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Sign Up</Link>
+              <Link to="/products">Products</Link>
+            </div>
+          )}
+        </nav>
+        <hr />
+      </div>
+    )
+  }
+}
 
 /**
  * CONTAINER
@@ -41,8 +51,12 @@ const mapState = (state) => {
 
 const mapDispatch = (dispatch) => {
   return {
-    handleClick() {
+    handleClick: () => {
       dispatch(logout())
+    },
+    load: () => {
+      console.log('loading data')
+      dispatch(getProducts())
     },
   }
 }
