f0dd6c1f9415a7734adf0adad63d4f08b1258e9e
katthartic <katt.baum@gmail.com>
2020-05-02 09:43:41 -0400

    Add admin to one user account

--- script/seed.js
+++ script/seed.js
@@ -10,7 +10,7 @@ async function seed() {
   console.log('db synced!')
 
   const users = await Promise.all([
-    User.create({email: 'cody@email.com', password: '123'}),
+    User.create({email: 'cody@email.com', password: '123', admin: true}),
     User.create({email: 'murphy@email.com', password: '123'}),
   ])
 
