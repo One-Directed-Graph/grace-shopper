e32c7dd53d97886e745355ef6840df5197e1fcfd
katthartic <katt.baum@gmail.com>
2020-05-02 09:42:56 -0400

    Add fetch users for admin route

--- client/routes.js
+++ client/routes.js
@@ -38,9 +38,7 @@ class Routes extends Component {
           {isLoggedIn && (
             <Switch>
               {/* Routes placed here are only available after logging in */}
-
               <Route path="/home" component={UserHome} />
-              <Route path="/home" component={AdminHome} />
             </Switch>
           )}
           {/* Displays our Login component as a fallback */}
