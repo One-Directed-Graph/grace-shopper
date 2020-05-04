29615b3fb66e35538814da4bf25b411fee0f5382
katthartic <katt.baum@gmail.com>
2020-05-04 17:34:43 -0400

    Add post, put, delete routes

--- server/api/users.js
+++ server/api/users.js
@@ -1,6 +1,9 @@
+const express = require('express')
 const router = require('express').Router()
 const {User} = require('../db/models')
 module.exports = router
+
+router.use(express.json())
 
 router.get('/', async (req, res, next) => {
   console.log('nmnmnmnmnmnmnmnmnmnmnmnmnmnmnmnmnmnmnmn')
@@ -11,21 +14,40 @@ router.get('/', async (req, res, next) => {
       // send everything to anyone who asks!
       attributes: ['id', 'email'],
     })
-    res.json(users)
+    res.send(users)
   } catch (err) {
     next(err)
   }
 })
 
-router.get('/user-list', async (req, res, next) => {
+router.get('/user-list', (req, res, next) => {
   console.log('getting user list for admin')
+  User.findAll({
+    where: {admin: false},
+    attributes: ['id', 'email', 'admin'],
+  })
+    .then((users) => res.send(users))
+    .catch(next)
+})
+
+router.post('/user-list', (req, res, next) => {
+  User.create(req.body)
+    .then((student) => res.status(201).send(student))
+    .catch(next)
+})
+
+router.put('/user-list', (req, res, next) => {
+  const {change} = req.body
+  User.findByPk(req.body.id)
+    .then((student) => student.update(change))
+    .then((student) => res.send(student))
+})
+
+router.delete('/user-list', async (req, res, next) => {
   try {
-    const users = await User.findAll({
-      where: {admin: false},
-      attributes: ['id', 'email', 'admin'],
-    })
-    res.json(users)
-  } catch (err) {
-    next(err)
+    await User.destroy({where: {id: req.body.id}})
+    res.status(204).end()
+  } catch (ex) {
+    next(ex)
   }
 })
