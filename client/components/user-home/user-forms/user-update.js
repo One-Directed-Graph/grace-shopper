406658c075f506d882f2e295b53ac3f9b8c08544
katthartic <katt.baum@gmail.com>
2020-05-07 14:12:20 -0400

    complete and functional

--- client/components/user-home/user-forms/user-update.js
+++ client/components/user-home/user-forms/user-update.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-import {Link, HashRouter, Route, Switch} from 'react-router-dom'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import {updateUser, removeUser} from '../../../store'
@@ -9,15 +8,16 @@ import {updateUser, removeUser} from '../../../store'
 /**
  * COMPONENT
  */
-const UserUpdate = ({userToUpdate, handleChange, error}) => {
+const UserUpdate = ({userToUpdate, handleChange, handleDelete, error}) => {
   return (
     <div id="user-update-form-div">
-      <div id="user-udpdate-checkboxes">
+      <div id="user-udpdate-elements">
         <Form.Check
           inline
           type="checkbox"
           name="admin"
-          label="Admin"
+          label="Admin User"
+          width={200}
           checked={userToUpdate.admin}
           onChange={(evt) => handleChange(userToUpdate, evt.target.name)}
         />
@@ -26,9 +26,17 @@ const UserUpdate = ({userToUpdate, handleChange, error}) => {
           type="checkbox"
           name="pwReset"
           label="Require Password Reset"
+          width={200}
           checked={userToUpdate.pwReset}
           onChange={(evt) => handleChange(userToUpdate, evt.target.name)}
         />
+        <Button
+          inline="true"
+          variant="danger"
+          onClick={(evt) => handleDelete(userToUpdate.id, evt.target)}
+        >
+          Delete User
+        </Button>
       </div>
       {error && error.response && <div> {error.response.data} </div>}
     </div>
@@ -39,19 +47,26 @@ const UserUpdate = ({userToUpdate, handleChange, error}) => {
  * CONTAINER
  */
 
-const mapState = (state) => {
-  console.log('in user-update state', state)
-  return {
-    state,
-  }
-}
+const mapState = null
+// (state) => {
+//   console.log('in user-update state', state)
+//   return {
+//     state,
+//   }
+// }
 
 const mapDispatch = (dispatch) => {
   return {
     handleChange(user, evtName) {
+      console.log(`updating user ${user.id}`)
       const {id, admin, pwReset} = user
       const newValue = evtName === 'admin' ? !admin : !pwReset
       dispatch(updateUser({id, change: {[evtName]: newValue}}))
+    },
+    handleDelete(id, evt) {
+      console.log('targer', evt.target)
+      console.log(`updating user ${id}`)
+      dispatch(removeUser(id))
     },
   }
 }
@@ -61,9 +76,9 @@ export default connect(mapState, mapDispatch)(UserUpdate)
 /**
  * PROP TYPES
  */
-// AuthForm.propTypes = {
-//   name: PropTypes.string.isRequired,
-//   displayName: PropTypes.string.isRequired,
-//   handleSubmit: PropTypes.func.isRequired,
-//   error: PropTypes.object,
-// }
+UserUpdate.propTypes = {
+  userToUpdate: PropTypes.object.isRequired,
+  handleDelete: PropTypes.func.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  error: PropTypes.object,
+}
