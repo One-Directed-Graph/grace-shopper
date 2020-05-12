328c2b131429826764fcaac54aa52719559acb09
katthartic <katt.baum@gmail.com>
2020-05-11 22:40:04 -0400

    Add list to products list

--- client/components/user-home/user-admin-comps/product-list.js
+++ client/components/user-home/user-admin-comps/product-list.js
@@ -1,19 +1,68 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-import {Link, HashRouter, Route, Switch} from 'react-router-dom'
+import ListGroup from 'react-bootstrap/ListGroup'
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
+import CardGroup from 'react-bootstrap/CardGroup'
 
-class ProductList extends Component {
-  // componentDidMount(){
-  //     this.props.load()
-  // }
+/**
+ * COMPONENT
+ */
 
-  render() {
-    return <h3 className="user-home-comps">Products</h3>
-  }
+const ProductList = (props) => {
+  const products = props.products ? props.products : []
+  return (
+    <div className="user-home-comps">
+      <h3>Products</h3>
+      <ListGroup id="user-home-products">
+        {products.map((product) => (
+          <Card key={product.id} style={{width: '10rem'}}>
+            <Card.Img variant="top" src={product.img} />
+            <Card.Body>
+              <Card.Title>{product.title}</Card.Title>
+              <Button
+                variant="primary"
+                type="submit"
+                onClick={(ev) => console.log(ev.target)}
+              >
+                Update
+              </Button>
+            </Card.Body>
+          </Card>
+        ))}
+      </ListGroup>
+    </div>
+  )
 }
 
-const mapState = null
-const mapDispatch = null
+{
+  /* <ListGroupItem key={product.id} className="mx-auto">
+<h6>{product.title}</h6>
+<div
+  className="product-list-image"
+  style={{backgroundImage: 'url(' + product.img + ')'}}
+/>
+<Button
+  variant="primary"
+  type="submit"
+  onClick={(ev) => console.log(ev.target)}
+>
+  Update
+</Button>
+</ListGroupItem> */
+}
 
-export default connect(mapState, mapDispatch)(ProductList)
+/**
+ * CONTAINER
+ */
+const mapState = ({products}) => ({products})
+
+export default connect(mapState)(ProductList)
+
+/**
+ * PROP TYPES
+ */
+ProductList.propTypes = {
+  products: PropTypes.array,
+}
