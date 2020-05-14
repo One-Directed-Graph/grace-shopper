9cc4ff8f064f56d84372aa1c8b46ddd657a12c01
AleksandrKayner <AleksandrKayner6@gmail.com>
2020-05-14 01:57:19 -0400

    edit delete work

--- client/components/orders.js
+++ client/components/orders.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react'
 import {getOrders} from '../store'
 import {connect} from 'react-redux'
-import {Modal, Button, ListGroup} from 'react-bootstrap'
+import {Form, Modal, Button, ListGroup} from 'react-bootstrap'
 import {MyVerticallyCenteredModal} from './modelPopup'
+import {destroyItem, getItems, editItem} from '../store/orderItems'
 class Orders extends Component {
-  constructor() {
+  constructor(props) {
+    console.log('propspropsprops', props)
     super()
-    // this.state = {
-    //   modalShow: false,
-    // }
+    this.state = {
+      quantity: 1,
+    }
     // this.setModalShow = this.setModalShow.bind(this)
   }
   // setModalShow(input) {
@@ -16,12 +18,19 @@ class Orders extends Component {
   //   this.setState({modalShow: input})
   //   console.log(this.state.modalShow)
   // }
+  componentDidUpdate(prevProps) {
+    if (prevProps.orderItems.length !== this.props.orderItems.length) {
+      this.props.load()
+    }
+    console.log(prevProps.orderItems.length, this.props.orderItems.length)
+  }
   componentDidMount() {
     this.props.load()
   }
   render() {
     const {orders, user} = this.props
-    let a = []
+    const {quantity} = this.state
+    let arrayOfItems = []
     //const {orderitems} = this.props.orders
     if (orders && user) {
       console.log('gfgfgfgfgfgfgfgfgf', orders.id, orders)
@@ -30,12 +39,16 @@ class Orders extends Component {
         : ''
 
       //userOrders ? userOrders.map((item) => item.orderitems) : ''
-      //console.log('ordersitems 1111', orderItems)
+      console.log('ordersitems 1111', orders[0])
       console.log('userorders 222222', userOrders.length, userOrders[0])
-      a = userOrders[0]
-      if (a) {
-        console.log('aaaaaaaaaaaaaaaaaaaa', a.orderitems.length, typeof a)
-      }
+      arrayOfItems = userOrders[0]
+      //   if (arrayOfItems) {
+      //     console.log(
+      //       'aaaaaaaaaaaaaaaaaaaa',
+      //       arrayOfItems.orderitems.length,
+      //       typeof a
+      //     )
+      //   }
     }
     //const {modalShow} = this.props
     //const {setModalShow} = this
@@ -53,14 +66,19 @@ class Orders extends Component {
     // )
     return (
       <div>
-        {a ? <h1> Cart ({a.orderitems.length} )</h1> : ''}
-        <ul className="listgrp">
-          {a
-            ? a.orderitems.map((item, idx) => {
+        {arrayOfItems ? <h1> Cart ({arrayOfItems.orderitems.length} )</h1> : ''}
+        <ul>
+          {arrayOfItems
+            ? arrayOfItems.orderitems.map((item, idx) => {
                 //console.log('lalalala', item[idx])
 
                 return (
-                  <ListGroup horizontal="sm" className="my-2" key={item.id}>
+                  <ListGroup
+                    horizontal="sm"
+                    className="my-2"
+                    key={item.id}
+                    id="listgrp"
+                  >
                     <ListGroup.Item>
                       {
                         <img
@@ -72,7 +90,41 @@ class Orders extends Component {
                     </ListGroup.Item>
                     <ListGroup.Item>
                       <p>Quantity</p>
-                      {item.quantity}
+                      <p>{item.quantity}</p>
+                      <Form
+                        style={{width: '100%'}}
+                        onSubmit={(e) => e.preventDefault()}
+                        className="colpic"
+                      >
+                        <Button
+                          onClick={(e) => {
+                            if (quantity * 1 > 0) {
+                              this.setState({quantity: quantity - 1})
+                            }
+                            if (quantity * 1 <= 0) {
+                              console.log('quantityt', quantity, item.id)
+                              return this.props.destroyItems(item.id)
+                            }
+                          }}
+                        >
+                          -
+                        </Button>
+                        <Form.Control
+                          style={{width: '50px'}}
+                          type="number"
+                          placeholder="add qvantity"
+                          onChange={(e) => {
+                            this.setState({quantity: e.target.value})
+                          }}
+                        />
+                        <Button
+                          onClick={(e) => {
+                            this.props.change(item.id, quantity)
+                          }}
+                        >
+                          +
+                        </Button>
+                      </Form>
                     </ListGroup.Item>
                     <ListGroup.Item>
                       <p>Price</p>
@@ -82,23 +134,31 @@ class Orders extends Component {
                 )
               })
             : ''}
-          <Button>add</Button>
         </ul>
       </div>
     )
   }
 }
 
-const mapState = ({orders, user}) => {
+const mapState = ({orders, user, orderItems}) => {
   return {
     orders,
     user,
+    orderItems,
   }
 }
 const mapDispatch = (dispatch) => {
   return {
     load: () => {
       dispatch(getOrders())
+      dispatch(getItems())
+    },
+    destroyItems: (id) => {
+      console.log('gogoggogogogogog')
+      dispatch(destroyItem(id))
+    },
+    change: (id, qv) => {
+      dispatch(editItem(id, qv))
     },
   }
 }
