1d91bb6a41983d9a66b05c0003a1ed0022dd2beb
AleksandrKayner <AleksandrKayner6@gmail.com>
2020-05-19 16:26:29 -0400

    sort fixed

--- client/components/helpers.js
+++ client/components/helpers.js
@@ -1,7 +1,6 @@
 export function ratingStars(ratings) {
   const styleStar = 'fa fa-star'
   let rating = ratings
-  console.log('rating', rating)
   if (Array.isArray(ratings)) {
     rating =
       ratings.reduce((acc, curr) => acc + curr.rating, 0) / ratings.length
@@ -9,11 +8,9 @@ export function ratingStars(ratings) {
   } else {
     rating = ratings
   }
-  console.log('rating', rating)
   if (rating > 0) {
     const diff = new Array(5 - rating).fill(styleStar)
     const starArr = new Array(rating).fill(`${styleStar} checked`)
-    console.log('diff and startArr', diff, starArr)
     return [...starArr, ...diff]
   }
   return []
