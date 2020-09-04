94288f79516c97c8bcf000d3e436487704229cb9
AleksandrKayner <AleksandrKayner6@gmail.com>
2020-09-03 21:36:12 -0400

    footer update and logo

--- client/components/home/Footer.js
+++ client/components/home/Footer.js
@@ -3,74 +3,55 @@ import React from 'react'
 export const Footer = () => {
   return (
     <div>
-      <footer className="footer_dark">
-        <div className="footer_top">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-3 col-md-6 col-sm-12">
-                <div className="widget">
-                  <div className="footer_logo">
+      <footer class="footer_dark">
+        <div class="footer_top">
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-3 col-md-6 col-sm-12">
+                <div class="widget">
+                  <div class="footer_logo">
                     <a href="#">
                       <img
-                        src="/images/imageSite/maskerade_logo_white.png"
+                        src="/images/imageSite/maskerade_logo_black.png"
                         alt="logo"
                       />
                     </a>
                   </div>
-                  <p>
-                    If you are going to use of Lorem Ipsum need to be sure there
-                    isn't hidden of text
-                  </p>
                 </div>
               </div>
-              <div className="col-lg-2 col-md-3 col-sm-6">
-                <div className="widget">
-                  <h6 className="widget_title">Useful Links</h6>
-                  <ul className="widget_links">
+              <div class="col-lg-2 col-md-3 col-sm-6">
+                <div class="widget">
+                  <h6 class="widget_title">Useful Links</h6>
+                  <ul class="widget_links">
                     <li>
-                      <a href="#">About Us</a>
+                      <a href="#">Products</a>
                     </li>
                     <li>
-                      <a href="#">FAQ</a>
-                    </li>
-                    <li>
-                      <a href="#">Location</a>
-                    </li>
-                    <li>
-                      <a href="#">Affiliates</a>
-                    </li>
-                    <li>
-                      <a href="#">Contact</a>
+                      <a href="#">Contact Us</a>
                     </li>
                   </ul>
                 </div>
               </div>
-              <div className="col-lg-2 col-md-3 col-sm-6">
-                <div className="widget">
-                  <h6 className="widget_title">Category</h6>
-                  <ul className="widget_links">
+              <div class="col-lg-2 col-md-3 col-sm-6">
+                <div class="widget">
+                  <h6 class="widget_title">Category</h6>
+                  <ul class="widget_links">
                     <li>
-                      <a href="#">Men</a>
+                      <a href="#">Handmade</a>
                     </li>
                     <li>
-                      <a href="#">Woman</a>
+                      <a href="#">Fashion</a>
                     </li>
                     <li>
-                      <a href="#">Kids</a>
-                    </li>
-                    <li>
-                      <a href="#">Best Saller</a>
-                    </li>
-                    <li>
-                      <a href="#">New Arrivals</a>
+                      <a href="#">Medical</a>
                     </li>
                   </ul>
                 </div>
               </div>
-              <div className="col-lg-2 col-md-6 col-sm-6">
-                <div className="widget">
-                  <h6 className="widget_title">My Account</h6>
-                  <ul className="widget_links">
+              <div class="col-lg-2 col-md-6 col-sm-6">
+                <div class="widget">
+                  <h6 class="widget_title">My Account</h6>
+                  <ul class="widget_links">
                     <li>
                       <a href="#">My Account</a>
                     </li>
@@ -89,21 +70,21 @@ export const Footer = () => {
                   </ul>
                 </div>
               </div>
-              <div className="col-lg-3 col-md-4 col-sm-6">
-                <div className="widget">
-                  <h6 className="widget_title">Contact Info</h6>
-                  <ul className="contact_info contact_info_light">
+              <div class="col-lg-3 col-md-4 col-sm-6">
+                <div class="widget">
+                  <h6 class="widget_title">Contact Info</h6>
+                  <ul class="contact_info contact_info_light">
                     <li>
-                      <i className="ti-location-pin"></i>
+                      <i class="ti-location-pin"></i>
                       <p>123 Street, Old Trafford, New South London , UK</p>
                     </li>
                     <li>
-                      <i className="ti-email"></i>
-                      <a href="mailto:info@sitename.com">info@sitename.com</a>
+                      <i class="ti-email"></i>
+                      <a href="mailto:info@sitename.com">info@maskerade.com</a>
                     </li>
                     <li>
-                      <i className="ti-mobile"></i>
-                      <p>+ 457 789 789 65</p>
+                      <i class="ti-mobile"></i>
+                      <p>+ 123-456-7890</p>
                     </li>
                   </ul>
                 </div>
@@ -111,12 +92,12 @@ export const Footer = () => {
             </div>
           </div>
         </div>
-        <div className="bottom_footer border-top-tran">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12">
-                <p className="mb-md-0 text-center text-md-left">
-                  © 2020 All Rights Reserved by Bestwebcreator
+        <div class="bottom_footer border-top-tran">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-12">
+                <p class="mb-md-0 text-center text-md-left">
+                  © 2020 All Rights Reserved by Maskerade
                 </p>
               </div>
             </div>
