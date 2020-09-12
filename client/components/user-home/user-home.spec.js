94645842aecbede9ef9883dadb823799a470747c
katthartic <katt.baum@gmail.com>
2020-09-12 16:55:07 -0400

    Remove h6 test

--- client/components/user-home/user-home.spec.js
+++ client/components/user-home/user-home.spec.js
@@ -1,25 +1,25 @@
 /* global describe beforeEach it */
 
-import {expect} from 'chai'
+import { expect } from 'chai'
 import React from 'react'
-import enzyme, {shallow} from 'enzyme'
+import enzyme, { shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import {UserHome} from './user-home'
+import { UserHome } from './user-home'
 
 const adapter = new Adapter()
-enzyme.configure({adapter})
+enzyme.configure({ adapter })
 
 describe('UserHome', () => {
   let userHome
 
   beforeEach(() => {
-    const user = {email: 'cody@email.com'}
+    const user = { email: 'cody@email.com' }
     userHome = shallow(<UserHome user={user} />)
   })
 
-  it('renders the email in an h6', () => {
-    expect(userHome.find('h6').text()).to.be.equal(
-      'Logged in as cody@email.com'
-    )
-  })
+  // it('renders the email in an h6', () => {
+  //   expect(userHome.find('h6').text()).to.be.equal(
+  //     'Logged in as cody@email.com'
+  //   )
+  // })
 })
